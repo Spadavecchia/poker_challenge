d25c7268b5cb35809e0db83f29bc64919cdf7e9d
Antonio Abella <antonioabella@designiostecnologicos.com>
2017-11-28 09:04:42 -0400

    feat: get card value

--- lib/poker_challenge.ex
+++ lib/poker_challenge.ex
@@ -9,4 +9,20 @@ defmodule PokerChallenge do
   def suit([_ | s]) do
     to_string s
   end
+
+  @doc """
+  Value of a card
+  """
+  def value([v | _]) do
+    nv = v |> to_string() |> String.to_integer()
+    case nv do
+      x when x in 50..58 -> x - 48
+      84 -> 10
+      74 -> 11
+      81 -> 12
+      75 -> 13
+      65 -> 14
+      _ -> nil
+    end
+  end
 end
--- test/poker_challenge_test.exs
+++ test/poker_challenge_test.exs
@@ -8,4 +8,20 @@ defmodule PokerChallengeTest do
     assert PokerChallenge.suit('9D') == "D"
     assert PokerChallenge.suit('9S') == "S"
   end
+
+  test "value value of a card" do
+    assert PokerChallenge.value('2C') == 2
+    assert PokerChallenge.value('3C') == 3
+    assert PokerChallenge.value('4H') == 4
+    assert PokerChallenge.value('5H') == 5
+    assert PokerChallenge.value('6D') == 6
+    assert PokerChallenge.value('7D') == 7
+    assert PokerChallenge.value('8D') == 8
+    assert PokerChallenge.value('9S') == 9
+    assert PokerChallenge.value('TC') == 10
+    assert PokerChallenge.value('JH') == 11
+    assert PokerChallenge.value('QD') == 12
+    assert PokerChallenge.value('KS') == 13
+    assert PokerChallenge.value('AS') == 14
+  end
 end
