424f57ea8565fba185c736b789e9b88474b4b068
Antonio Abella <antonioabella@designiostecnologicos.com>
2017-11-28 07:48:16 -0400

    First Commit

---
+++ lib/poker_challenge.ex
@@ -0,0 +1,18 @@
+defmodule PokerChallenge do
+  @moduledoc """
+  Documentation for PokerChallenge.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> PokerChallenge.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/poker_challenge_test.exs
@@ -0,0 +1,8 @@
+defmodule PokerChallengeTest do
+  use ExUnit.Case
+  doctest PokerChallenge
+
+  test "greets the world" do
+    assert PokerChallenge.hello() == :world
+  end
+end
