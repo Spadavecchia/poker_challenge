acd87e9ecccb98355b0ab1e43f3229390ba90f4c
Antonio Abella <antonioabella@designiostecnologicos.com>
2017-12-01 04:25:17 -0400

    feat: check hand winner in a game

--- lib/poker_challenge.ex
+++ lib/poker_challenge.ex
@@ -2,6 +2,17 @@ defmodule PokerChallenge do
   @moduledoc """
   A Poker Game
   """
+  import Hand
 
+  def compare_card_values([], []), do: "Draw!"
+
+  def winner(w, b) do
+    cond do
+      hand_value(w) > hand_value(b) -> "White wins"
+      hand_value(w) < hand_value(b) -> "Black wins"
+      true                          -> compare_card_values(w, b)
+    end
+  end
 end
+
 
--- test/poker_challenge_test.exs
+++ test/poker_challenge_test.exs
@@ -2,4 +2,19 @@ defmodule PokerChallengeTest do
   use ExUnit.Case
   doctest PokerChallenge
 
+  @high_card ['8C', '5D', '4D', '3S', '2H']
+  @pair_low_value ['6C', '5D', '5H', '4D', '3S']
+  @hand_03 ['6C', '5D', '5H', '4D', '4S']
+  @hand_04 ['5C', '5D', '5H', '4D', '3S']
+  @hand_05 ['6C', '5D', '4H', '3D', '2S']
+  @hand_06 ['AC', 'KD', 'QH', 'JD', 'TS']
+  @hand_07 ['8C', '5C', '4C', '3C', '2C']
+  @hand_08 ['5C', '5D', '5H', '4D', '4S']
+  @hand_09 ['2H', '2S', '2C', '2D', '7D']
+  @hand_10 ['2H', '3H', '6H', '5H', '4H']
+
+  test "check the hand winner" do
+    assert PokerChallenge.winner(@pair_low_value, @high_card) == "White wins"
+    assert PokerChallenge.winner(@high_card, @pair_low_value) == "Black wins"
+  end
 end
