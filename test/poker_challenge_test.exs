93ad1231e891e1b03ad836dacd978b152c1a49b1
Antonio Abella <antonioabella@designiostecnologicos.com>
2017-12-01 05:33:10 -0400

    test: add a few games to test

--- test/poker_challenge_test.exs
+++ test/poker_challenge_test.exs
@@ -2,20 +2,21 @@ defmodule PokerChallengeTest do
   use ExUnit.Case
   doctest PokerChallenge
 
-  @high_card ['8C', '5D', '4D', '3S', '2H']
-  @pair_low_value ['6C', '5D', '5H', '4D', '3S']
-  @hand_03 ['6C', '5D', '5H', '4D', '4S']
-  @hand_04 ['5C', '5D', '5H', '4D', '3S']
-  @hand_05 ['6C', '5D', '4H', '3D', '2S']
-  @hand_06 ['AC', 'KD', 'QH', 'JD', 'TS']
-  @hand_07 ['8C', '5C', '4C', '3C', '2C']
-  @hand_08 ['5C', '5D', '5H', '4D', '4S']
-  @hand_09 ['2H', '2S', '2C', '2D', '7D']
-  @hand_10 ['2H', '3H', '6H', '5H', '4H']
+  @high_card_low        ['8C', '5D', '4D', '3S', '2H']
+  @high_card            ['8C', '7C', '4D', '3S', '2H']
+  @pair_low             ['6C', '5D', '5H', '4D', '2S']
+  @pair                 ['6C', '5D', '5H', '4D', '3S']
+  @three_of_a_kind_low  ['5C', '5D', '5H', '4D', '3S']
+  @three_of_a_kind      ['6C', '6D', '6H', '4D', '2S']
+  @three_of_a_kind_high ['6C', '6D', '6H', '4D', '3S']
 
   test "check the hand winner" do
-    assert PokerChallenge.winner(@pair_low_value, @high_card) == "White wins"
-    assert PokerChallenge.winner(@high_card, @pair_low_value) == "Black wins"
-    assert PokerChallenge.winner(@high_card, @high_card) == "Draw!"
+    assert PokerChallenge.winner(@pair_low, @high_card)                   == "White wins"
+    assert PokerChallenge.winner(@high_card, @pair_low)                   == "Black wins"
+    assert PokerChallenge.winner(@high_card, @high_card)                  == "Draw!"
+    assert PokerChallenge.winner(@high_card, @high_card_low)              == "White wins"
+    assert PokerChallenge.winner(@pair_low, @pair)                        == "Black wins"
+    assert PokerChallenge.winner(@three_of_a_kind_high, @three_of_a_kind) == "White wins"
+    assert PokerChallenge.winner(@three_of_a_kind, @three_of_a_kind_low)  == "White wins"
   end
 end
